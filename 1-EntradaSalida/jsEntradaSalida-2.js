6da1472244e1839f4e76ce7bd9d5f276e17d20d3
BustamanteMathias <48842768+BustamanteMathias@users.noreply.github.com>
2019-03-24 16:39:52 -0300

    Add files via upload

---
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostrar()
+{
+    var Dato;
+        Dato = prompt("Insertar DATO");
+
+        alert(Dato);
+}
+
